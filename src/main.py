553bd0d7b24a14c808e26bb893133a2a61c00058
Revixa1 <txguimont@hotmail.com>
2023-09-20 22:28:04 -0400

    ok

--- src/main.py
+++ src/main.py
@@ -1,1 +1,12 @@
 # Revixa1 2023
+
+def main():
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
--- src/rule.py
+++ src/rule.py
@@ -1,20 +1,47 @@
-import compiler
+import Calc
+import sympy
+from process_latex import process_sympy
 
 
 
-class Rule
 
-    def __Init__(self,equation)
-        self.equation=compiler.parse(equation)
+class Rule():
+
+
+
+    def __Init__(self,newequation: str):
+        self.Calc=Calc()
+        self.setEquation(newequation)
+        self.setRuleElements(self.getEquation())
         
+  #  def setState(newstate: int):
+  #      self.state: int=newstate
+    
+    def setEquation(newequation: str):
+        self.equation: str=process_sympy(newequation)
+
+    def getEquation():
+        return self.equation: str
+
+    #def setConstants(newdictionaryConstants):
+    #    self.dictionaryConstants=newdictionaryConstants
+
+   # def setInputVariables(newdictionaryVariables):
+   #     self.dictionaryVariables=newdictionaryVariables
+
+    def setRuleElements(newequation: str):
+       # self.RuleElements=newequation.split('+','-','*','/','**',)
 
 
-class MaxingRule(Rule)
+
+    def getRuleElements():
+       # self.RuleElements=dict(self.dictionaryConstants)
+       # self.RuleElements.update(dictionaryVariables)
+        return self.RuleElements
+
+    
+    def ExecuteRule66():
+        sympify(self.getEquation).evalf()
 
 
-class MinimisingRule(Rule)
 
-
-
-class PinedRule(Rule)
-        
